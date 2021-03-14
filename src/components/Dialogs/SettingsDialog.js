6e8d0c6e39a29d95a269b1e780e14eb5820a9a69
T0jgli <kovalikadam00@gmail.com>
2021-03-14 12:54:45 +0100

    new feature: custom profile picture

--- src/components/Dialogs/SettingsDialog.js
+++ src/components/Dialogs/SettingsDialog.js
@@ -1,25 +1,58 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {
     Button, Dialog, DialogContent, DialogTitle, DialogActions, TextField,
-    IconButton, Tooltip, Grow
+    IconButton, Tooltip, Grow, Avatar, makeStyles, Backdrop, CircularProgress
 } from '@material-ui/core'
 import { selectlanguage, setsnackbar } from '../../lib/AppSlice'
 import { selectUser } from '../../lib/userSlice'
-import db from '../../lib/firebase'
+import db, { storage } from '../../lib/firebase'
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import DeleteIcon from '@material-ui/icons/Delete';
 import firebase from "firebase/app"
 import { useDispatch, useSelector } from 'react-redux'
+import EditIcon from '@material-ui/icons/Edit';
+
+const useStyles = makeStyles((theme) => ({
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+}));
 
 const SettingsDialog = ({ setsettingsdialog, settingsdialog, setconfirmprompt }) => {
+    const classes = useStyles();
+
     const dispatch = useDispatch()
+    const photoChangeRef = useRef(null)
     const language = useSelector(selectlanguage)
     const user = useSelector(selectUser)
     const [newusername, setnewusername] = useState(user.displayname)
+    const [newimage, setnewimage] = useState(null)
+    const [uploadvalue, setuploadvalue] = useState(0)
+    const [loading, setloading] = useState(false)
+
 
     const u = firebase.auth().currentUser
 
     const handleeditusernamedone = async () => {
+        if (newimage) {
+            const uploadtask = storage.ref(`photoImages/${user.uid}__currentPhoto`).put(newimage);
+            setloading(true)
+            uploadtask.on("state_changed", snapshot => {
+                setuploadvalue((snapshot.bytesTransferred / newimage.size) * 100)
+            }, error => console.log(error), async () => {
+                setloading(false)
+                const newPhotoUrl = await storage.ref(`photoImages/${user.uid}__currentPhoto`).getDownloadURL()
+                db.collection("users").doc(user.uid).update({
+                    photoUrl: newPhotoUrl
+                })
+                u.updateProfile({
+                    photoURL: newPhotoUrl,
+                })
+                if (newusername === user.displayname || !newusername)
+                    window.location.reload(false)
+            })
+        }
         if (!newusername) {
             setnewusername(user.displayname)
             return dispatch(setsnackbar({
@@ -44,6 +77,7 @@ const SettingsDialog = ({ setsettingsdialog, settingsdialog, setconfirmprompt })
                 console.error(error)
             }
         }
+
     }
 
 
@@ -59,42 +93,83 @@ const SettingsDialog = ({ setsettingsdialog, settingsdialog, setconfirmprompt })
     }
 
     return (
-        <Dialog TransitionComponent={Grow} open={settingsdialog} onClose={() => setsettingsdialog(false)}>
-            <DialogContent>
-                <DialogTitle style={{ margin: "5px" }}>
-                    {language === "hu" ? ("Beállítások") : ("Settings")}
-                </DialogTitle>
-                <ArrowDropDownIcon />
-                <form style={{ marginTop: "20px" }} onSubmit={(e) => { e.preventDefault(); handleeditusernamedone(); setsettingsdialog(false) }}>
-                    <TextField label={language === "hu" ? ("Felhasználónév") : ("Username")}
-                        variant="outlined" value={newusername} onChange={(e) => { setnewusername(e.target.value) }} />
-                </form>
-                <br />
-                <Tooltip title={language === "hu" ? ("Fiók törlése") : ("Delete account")}>
-                    <IconButton onClick={() => {
-                        setconfirmprompt({
-                            en: "Are you sure you want to delete this user account?",
-                            hu: "Biztosan törlöd a felhasználói fiókot?",
-                            open: true,
-                            enter: handledeleteuser,
-                        })
-                    }} style={{ backgroundColor: "red", margin: "20px", color: "rgb(225, 225, 225)" }}>
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
+        <>
+            {loading && (
+                <Backdrop className={classes.backdrop} open={loading}>
+                    <CircularProgress color="inherit" variant="static" value={uploadvalue} />
+                </Backdrop>
+            )}
 
-            </DialogContent>
-            <DialogActions >
-                <Button style={{ color: "rgb(255, 255, 255, 0.5)", fontWeight: "bolder" }}
-                    onClick={() => setsettingsdialog(false)}>{language === "hu" ? ("Mégse") : ("Cancel")}
-                </Button>
+            <Dialog TransitionComponent={Grow} open={settingsdialog} onClose={() => setsettingsdialog(false)}>
+                <DialogContent>
+                    <DialogTitle style={{ margin: "5px" }}>
+                        {language === "hu" ? ("Beállítások") : ("Settings")}
+                    </DialogTitle>
+                    <ArrowDropDownIcon />
+                    <div className="settingsdialog__avatardiv">
+                        <Avatar src={newimage ? URL.createObjectURL(newimage) : user.photo} className="settingsdialog__avatar" />
+                        <div className="editicon">
+                            <EditIcon onClick={() => photoChangeRef.current.click()} />
+                        </div>
 
-                <Button style={{ color: "rgb(255, 255, 255, 1)", fontWeight: "bolder" }}
-                    onClick={() => { handleeditusernamedone(); setsettingsdialog(false) }}>{language === "hu" ? ("Kész") : ("Done")}</Button>
+                        <input type="file" ref={photoChangeRef} accept="image/*" style={{ display: "none" }} onChange={(e) => {
+                            if (e.target.files[0]) {
+                                if (!e.target.files[0].type.includes("image")) return dispatch(setsnackbar({
+                                    snackbar: {
+                                        open: true,
+                                        type: "error",
+                                        filesizeerror: true,
+                                        hu: `Csak kép lehet a profilképed`,
+                                        en: `Your profile picture should be a picture`,
+                                    }
+                                }))
+                                if (e.target.files[0]?.size < 52428800) {
+                                    setnewimage(e.target.files[0])
+                                }
+                                else
+                                    dispatch(setsnackbar({
+                                        snackbar: {
+                                            open: true,
+                                            type: "error",
+                                            filesizeerror: true,
+                                            hu: `A fotó mérete ${formatBytes(e.target.files[0].size)}, amely meghaladja a maximális méretet! (50 MB)`,
+                                            en: `Photo size is ${formatBytes(e.target.files[0].size)}, which exceeds the maximum size! (50 MB)`,
+                                        }
+                                    }))
+                            }
 
-            </DialogActions>
-        </Dialog>
+                        }} />
+                    </div>
+                    <form style={{ marginTop: "20px" }} onSubmit={(e) => { e.preventDefault(); handleeditusernamedone(); setsettingsdialog(false) }}>
+                        <TextField label={language === "hu" ? ("Felhasználónév") : ("Username")}
+                            variant="outlined" value={newusername} onChange={(e) => { setnewusername(e.target.value) }} />
+                    </form>
+                    <br />
+                    <Tooltip title={language === "hu" ? ("Fiók törlése") : ("Delete account")}>
+                        <IconButton onClick={() => {
+                            setconfirmprompt({
+                                en: "Are you sure you want to delete this user account?",
+                                hu: "Biztosan törlöd a felhasználói fiókot?",
+                                open: true,
+                                enter: handledeleteuser,
+                            })
+                        }} style={{ backgroundColor: "red", margin: "20px", color: "rgb(225, 225, 225)" }}>
+                            <DeleteIcon />
+                        </IconButton>
+                    </Tooltip>
 
+                </DialogContent>
+                <DialogActions >
+                    <Button style={{ color: "rgb(255, 255, 255, 0.5)", fontWeight: "bolder" }}
+                        onClick={() => setsettingsdialog(false)}>{language === "hu" ? ("Mégse") : ("Cancel")}
+                    </Button>
+
+                    <Button style={{ color: "rgb(255, 255, 255, 1)", fontWeight: "bolder" }}
+                        onClick={() => { handleeditusernamedone(); setsettingsdialog(false) }}>{language === "hu" ? ("Kész") : ("Done")}</Button>
+
+                </DialogActions>
+            </Dialog>
+        </>
     )
 }
 
